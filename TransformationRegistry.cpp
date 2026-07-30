0051caf0ff6d17c04ad2304b90c3fdb6c3613b19
Azolile Mbanga <u24820360@tuks.co.za>
2026-07-30 13:22:51 +0200

    task 2 7th commit Transformation complete

--- TransformationRegistry.cpp
+++ TransformationRegistry.cpp
@@ -12,9 +12,24 @@ void TransformationRegistry::registerStep(string key, Transformation* prototype)
 }
 
 Transformation* TransformationRegistry::create(string key){
-    //
+    auto it = this->prototypes.find(key);
+    if (it == this->prototypes.end())
+    {
+        return nullptr;
+    }
+    
+    return this->prototypes[key]->clone();
 }
 
 TransformationRegistry::~TransformationRegistry(){
-    //
+    for (auto& pair : this->prototypes) {
+        delete pair.second;
+    }
+    this->prototypes.clear();
 }
+
+//1.6
+//Connector - This is the Abstract Product class.
+//CsvConnector - This is the Concrete Product class.
+//ConnectorFactory - This is the Abstract Factory class.
+//CsvFactory - This is the Concrete Factory class.
